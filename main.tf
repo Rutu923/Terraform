cee6cef04ecfbe45524a2e6b8094acfe2f94ecc5
Rutu923 <rutujamalpute@gmail.com>
2024-06-01 20:14:46 +0530

    abc

---
+++ main.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+     region = "us-east-2"  # Set your desired AWS region
+}
+resource "aws_instance" "this" {
+  ami                     = "ami-0dcc1e21636832c5d"
+  instance_type           = "m5.large"
+}
