4f4fe19bcf1e6b819e055e949ee73dbd64ceacc6
Rutu923 <164317050+Rutu923@users.noreply.github.com>
2024-06-06 16:32:11 +0530

    Create variables.tf

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,35 @@
+variable "this_aws_instance_ami" {
+     type = string
+}
+
+variable "this_aws_instance_type" {
+    type = string
+}
+variable "this_aws_instance_key_name" {
+    type = string
+}
+variable "this_aws_instance_availability_zone" {
+    type = string
+}
+variable "this_aws_instance_volume_size" {
+    type = number
+}
+
+variable "this_aws_instance_count" {
+    type = number
+} 
+variable "this_aws_instance_user_data_base64" {
+    type = bool
+} 
+variable "this_aws_instance_tags" {
+    type = string
+} 
+variable "this_aws_instance_subnet" {
+    type = string
+} 
+variable "this_aws_vpc_id" {
+    type = string
+}  
+variable "this_sg_vpc_id" {
+     type = string
+}
