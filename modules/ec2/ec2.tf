dc9c265a740d0b98d137b2741eac579b9cf485c5
Rutu923 <164317050+Rutu923@users.noreply.github.com>
2024-06-06 16:31:31 +0530

    Create ec2.tf

---
+++ modules/ec2/ec2.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "this_aws_instance" {
+  ami                    = var.this_aws_instance_ami
+  instance_type          = var.this_aws_instance_type
+  key_name               = var.this_aws_instance_key_name
+  vpc_security_group_ids =  [var.this_sg_vpc_id]
+//[var.this_aws_instance_sg_id]
+  availability_zone      = var.this_aws_instance_availability_zone
+  subnet_id = var.this_aws_instance_subnet 
+  root_block_device {
+    volume_size = var.this_aws_instance_volume_size
+  }
+  count = var.this_aws_instance_count
+  //user_data_base64 = var.this_aws_instance_user_data_base64
+    user_data = <<-EOF
+            #!/bin/bash
+    sudo -i
+    
+    EOF 
+
+
+
+  tags = {
+      
+      Name =  var.this_aws_instance_tags
+  }
+}
